e94aab5711f5c6802ac64a233d8c37f577d2de69
Caglar Yalcin <github@caglaryalcin.com>
2025-11-30 14:12:42 +0300

    Update encoding.ps1

--- encoding.ps1
+++ encoding.ps1
@@ -1,1 +1,1 @@
-[IO.File]::WriteAllText("$env:TEMP\i.ps1",(iwr https://raw.githubusercontent.com/caglaryalcin/ollama/main/install.ps1).Content,[Text.Encoding]::UTF8);&$env:TEMP\i.ps1
+[IO.File]::WriteAllText("$env:TEMP\i.ps1",(iwr https://raw.githubusercontent.com/caglaryalcin/ollama/main/set.ps1).Content,[Text.Encoding]::UTF8);&$env:TEMP\i.ps1
